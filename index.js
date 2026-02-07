bdbfa6ac0461640ed10bce24d78960d1c7e276c9
subhakantadev <subhakantadev@gmail.com>
2026-02-07 13:31:52 +0530

    9

--- index.js
+++ index.js
@@ -35,11 +35,3 @@ function loadUsers() {
       });
     });
 }
-
-console.log({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  db: process.env.DB_NAME,
-  hasPassword: !!process.env.DB_PASSWORD,
-});
